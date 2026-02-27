1612687fa8e4203aa30fca1ccb94308b817ec2a9
BogdanZarichnyy <bogdanzarichnyy@gmail.com>
2026-02-28 01:42:51 +0200

    fix logic user services

--- app/services/user.py
+++ app/services/user.py
@@ -66,16 +66,6 @@ class UserService:
     self,
     user_data: SignUpRequest
   ) -> UserDetailResponse:
-    # Перевірка email
-    existing_user = await self.repo.get_user_by_email(user_data.email)
-    if existing_user:
-      logger.warning(f"User creation failed. Email exists: {user_data.email}")
-      raise ExistsEmail(user_data.email)
-    # Перевірка username
-    existing_user_by_username = await self.repo.get_user_by_username(user_data.username)
-    if existing_user_by_username:
-        logger.warning(f"User creation failed. Username exists: {user_data.username}")
-        raise ExistsUsername(user_data.username)  # новий кастомний ексепшн
     hashed_password = (
       hash_password(user_data.password)
       if user_data.password
@@ -91,12 +81,14 @@ class UserService:
     try:
       user = await self.repo.create_user(user)
     except IntegrityError as e:
+      # Обробка унікальних ключів
       if "users_username_key" in str(e.orig):
-        logger.warning(f"User creation failed. Username exists: {user_data.username}")
-        raise ExistsUsername(user_data.username)
+          logger.warning(f"User creation failed. Username exists: {user_data.username}")
+          raise ExistsUsername(user_data.username)
       if "users_email_key" in str(e.orig):
-        logger.warning(f"User creation failed. Email exists: {user_data.email}")
-        raise ExistsEmail(user_data.email)
+          logger.warning(f"User creation failed. Email exists: {user_data.email}")
+          raise ExistsEmail(user_data.email)
+      # Інші помилки піднімаємо далі
       raise
     logger.info(f"User created id={user.id}")
     return UserDetailResponse.model_validate(user)
